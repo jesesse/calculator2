454a898ba8b0152cf95a0b38e560f7a368ac7399
jesse <jesse@legion.home>
2022-03-29 19:42:37 +0300

    Tällä hetkellä haasteen aoikeastaan vaan negatiiivisten lukujnen inputti: Mitenkä sais jokaisen luvun olemaan itsesssään negatiivinen? Eli periaatteessa aina kun paina operaattori nappia, niin edellinen lukupitäisi tallentaa sillä olevalla etumerkillä. Ja uuden luvun näppäillessä pitäisi pystyä vaita onko se negatiivinen vai ei

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const display = document.querySelector('.display');
 const digits = document.querySelector('.digits');
 
-for (let i = 1; i <= 12; i++){
+for (let i = 1; i <= 12; i++) {
     const btn = document.createElement('button');
     btn.classList.add('digit')
     btn.setAttribute('id', i);
@@ -20,53 +20,64 @@ document.getElementById('12').textContent = '+/-'
 let currentResult = null;
 let latestNumber = null;
 let finalRestul = null;
-let operation = null;
+let operation = false;
 let errorFlag = null;
-let negative = null;
+let negative = 0;
+let decimalFlag = 0;
 
 
 /*------------------OPERATION LOGICS-------------------------*/
 
 function sum(a, b) {
     result = (+a) + (+b)
-    if (!Number.isInteger(result)) return result.toFixed(4);
-    return result;
+    return parseFloat((result).toFixed(6));
 }
 
 function substract(a, b) {
     result = ((a) - (b));
-    if (!Number.isInteger(result)) return result.toFixed(4);
-    return result;
+    return parseFloat((result).toFixed(6));
 }
 
 function multiply(a, b) {
     result = ((a) * (b));
-    if (!Number.isInteger(result)) return result.toFixed(4);
-    return result;
+    return parseFloat((result).toFixed(6));
 }
 
 function divide(a, b) {
     result = ((a) / (b));
-    if (!Number.isInteger(result)) return result.toFixed(4);
-    return result;
+    return parseFloat((result).toFixed(6));
 }
 
-/*
-Adds the pressed digit to the display.
-Checks if the user wants a negative digit.
-*/
-function addDigit(e) {
-    if (e.target.textContent == '+/-') {
-        if (negative) {
-            display.textContent = display.textContent.slice(1, display.textContent.length)
-            negative = 0;
-        } else {
-            display.textContent = '-' + display.textContent;
-            negative = 1;
-        }
-        
-    } else display.textContent += e.target.textContent;
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+
+function operate(operation, a, b) {
+    if (operation === "+") return sum(a, b);
+    if (operation === "-") return substract(a, b);
+    if (operation === "*") return multiply(a, b);
+    if (operation === "/") return divide(a, b);
 }
+
+
+function calculate(operationBtnEvent) {
+    if (display.textContent === '' || display.textContent === '.' || display.textContent === '-') return;
+    let i = display.textContent.lastIndexOf(operation)
+
+    if (!operation || operation == '=') currentResult = parseFloat(display.textContent);
+    else if (i == display.textContent.length - 1) {
+        //if user has pressed an some oeprator button multiple times, do nothing
+    } else {
+        latestNumber = display.textContent.slice((i + 1), (display.textContent.length));
+        currentResult = operate(operation, currentResult, latestNumber)
+    }
+
+    display.textContent = currentResult;
+    if (!(operationBtnEvent.target.textContent == '=')) display.textContent += operationBtnEvent.target.textContent;
+
+    operation = operationBtnEvent.target.textContent;
+    decimalFlag = 0;
+}
+
 
 /*
 Clears all data
@@ -78,23 +89,47 @@ function clear() {
     finalRestul = null;
     operation = null;
     negative = null;
-    
 }
 
 /*
 Erases the last digit from the display
 */
 function erase() {
-    display.textContent = display.textContent.slice(0, display.textContent.length - 1)
+    display.textContent = display.textContent.slice(0, -1)
 }
 
-/*--------------------------------------------------------------------------------*/
+/*
+Adds the pressed digit to the display.
+Prevents the input of multiple decimal points. (Only one decimal point per number allowed. Decimal flag undone in the calculate function)
+Also checks if the user wants a negative digit or not.
+*/
+function addDigit(e) {
 
-document.querySelectorAll('.digit').forEach((digit) => {
-    digit.addEventListener("click", addDigit)
-});
+    if (e.target.textContent === '.') {
+        if (decimalFlag) return;
+        else decimalFlag = 1;
+    }
+
+    if (e.target.textContent == '+/-') {
+        if (negative) {
+            display.textContent = display.textContent.slice(1, display.textContent.length)
+            negative = 0;
+        } else {
+            display.textContent = '-' + display.textContent;
+            negative = 1;
+        }
+
+    } else display.textContent += e.target.textContent;
+}
+
+/*---------------------------ADD EVENT LISTENERS FOR BUTTONS-----------------------------------------------------*/
+
+document.querySelectorAll('.digit').forEach((digit) => digit.addEventListener("click", addDigit));
 
 document.querySelector('.clear').addEventListener("click", clear);
 
 document.querySelector('.delete').addEventListener("click", erase);
 
+document.querySelector('.equals').addEventListener("click", operate);
+
+document.querySelector('.operations').childNodes.forEach((operationBtn) => operationBtn.addEventListener("click", calculate));
